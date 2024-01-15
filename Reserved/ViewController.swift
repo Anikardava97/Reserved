47cbffdca51b0b0c4d4e7af5de6aecd5f4ac1a6b
Anikardava97 <145837570+Anikardava97@users.noreply.github.com>
2024-01-15 05:08:56 -0800

    remove storyboard

---
+++ Reserved/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reserved
+//
+//  Created by Ani's Mac on 15.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
