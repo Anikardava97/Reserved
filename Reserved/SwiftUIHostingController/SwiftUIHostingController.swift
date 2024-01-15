7e47fc92e677aa27cfbbae8d21c8b83489d9bf4e
Anikardava97 <145837570+Anikardava97@users.noreply.github.com>
2024-01-15 11:21:18 -0800

    Add Login Options View with multiple authentication methods - Implement Continue with Apple, Google, Email buttons - Reorganize file structure for better modularity and readability Refactor related views and components

--- Reserved/SwiftUIHostingController/SwiftUIHostingController.swift
+++ Reserved/SwiftUIHostingController/SwiftUIHostingController.swift
@@ -11,13 +11,13 @@ import SwiftUI
 class SwiftUIHostingController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let swiftUIView = LaunchScreenView()
         let hostingController = UIHostingController(rootView: swiftUIView)
-
+        
         addChild(hostingController)
         view.addSubview(hostingController.view)
-
+        
         hostingController.view.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
